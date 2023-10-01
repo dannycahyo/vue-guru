ded4872e591b91dd7c0c1b6796351746beb5d716
dannycahyo <dannydwicahyono@gmail.com>
2023-10-01 14:34:50 +0700

    feat: implement counter comp & conditional rendering on it

---
+++ src/components/Counter.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+function multiplyBy2() {
+  count.value *= 2
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <h2>{{ count  }}</h2>
+    <button type="button" @click="increment">count ++</button>
+    <button v-if="count >= 1" type="button" @click="multiplyBy2">count * 2</button>
+    <p>
+      Counter component
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
